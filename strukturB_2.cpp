216b7edfc5bfe5cdbc327d6192d985fb9a2f7328
halimbagusalhakim <halsat132@gmail.com>
2024-04-01 14:53:01 +0700

    menampilkan nim dan nama dan menyimpan

--- strukturB_2.cpp
+++ strukturB_2.cpp
@@ -4,11 +4,22 @@ using namespace std;
 struct DetailAlamat {
     string desa;
     string kota;
-}
+};
 
 struct Mahasiswa {
     string nim;
     string nama;
     string alamat;
     int umur;
+};
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa :";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa :";
+    cin >> mhs.nama;
+
+    
+
 }
